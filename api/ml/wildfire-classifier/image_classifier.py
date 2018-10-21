e549916bd3273f204ef711289f96b2d3febbe653
Juliana Akemi <37545707+julianakemi@users.noreply.github.com>
2018-10-21 19:06:24 -0300

    ML: Fixing syntax error on WildlifeClassifier

--- api/ml/wildfire-classifier/image_classifier.py
+++ api/ml/wildfire-classifier/image_classifier.py
@@ -6,18 +6,19 @@ from fastai.dataset import *
 from fastai.sgdr import *
 from fastai.plots import *
 
+
 class WildlifeClassifier:
     def __init__(self):
         self.arch = resnet34
         self.sz = 224
         self.path = 'mock-data/'
-        data = ImageClassifierData.from_paths(self.path, tfms=tfms_from_model(arch, sz))
+        data = ImageClassifierData.from_paths(self.path, tfms=tfms_from_model(self.arch, self.sz))
         self.learn = ConvLearner.pretrained(self.arch, data, precompute=True)
         self.learn.load('wildfire-model')
 
     def predict_single_image(self, image_path):
-        trn_tfms, val_tfms = tfms_from_model(self.arch, self.sz) # get transformations
+        _, val_tfms = tfms_from_model(self.arch, self.sz)  # get transformations
         im = val_tfms(open_image(image_path))
-        self.learn.precompute=False # We'll pass in a raw image, not activations
+        self.learn.precompute = False  # We'll pass in a raw image, not activations
         preds = self.learn.predict_array(im[None])
-        return np.exp(preds)[0, 1] # preds are log probabilities of classes
+        return np.exp(preds)[0, 1]  # preds are log probabilities of classes
